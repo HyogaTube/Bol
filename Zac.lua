356be6bb1ad2318802f844e5abd90c9ee12f7509
HyogaTube <lucas.palitao@gmail.com>
2015-06-17 13:27:20 -0300

    Fix skills alcances

--- Zac.lua
+++ Zac.lua
@@ -9,4 +9,10 @@
 
 5.--]]
 
-6.local ts = TargetSelector(TARGET_LOW_HP,600,DAMAGE_MAGIC,true) 
+6.local ts = TargetSelector(TARGET_LOW_HP,600,DAMAGE_MAGIC,true)
+
+9.local qrRange = 550 
+10.local wRange = 350 
+11.local eRange = 1150, 1250, 1350, 1450, 1550 
+12.local rRange = 300 
+
