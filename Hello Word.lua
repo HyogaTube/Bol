92fe09e37c1e77bc3fd1692a40c37bd23966745c
HyogaTube <lucas.palitao@gmail.com>
2015-06-17 13:22:54 -0300

    Update Version

--- Hello Word.lua
+++ Hello Word.lua
@@ -1,5 +1,12 @@
+Versão 1.4.8
+Crétidos Chronoos E Hyoga
+Relato de bug me add no skype
+Eu ri
+
+
 function SayHello ()
          -- Print to the chat area
+         
          
 PrintChat ("Hello World")
 end
